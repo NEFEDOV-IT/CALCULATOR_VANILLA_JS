ae0ec26ecafd61637670a66c70a839698dff0e63
NEFEDOV_IT <nefedov_it@mail.ru>
2021-11-28 21:35:23 +0300

    add new fix for operation and refactor code

--- main.js
+++ main.js
@@ -1,60 +1,56 @@
 const buttons = document.querySelectorAll('.num_btn')
 const div = document.getElementById('div')
-const multi= document.getElementById('multi')
+const multi = document.getElementById('multi')
 const sub = document.getElementById('sub')
 const sum = document.getElementById('sum')
 const answer = document.getElementById('answer')
 const deleteNumber = document.getElementById('delete')
 const reload = document.getElementById('reload')
 const result = document.getElementById('result')
-let operation
-let one
-let two
+let operation = undefined
+let operandOne = null
+let operandTwo = null
 
 for (let button of buttons) {
-    button.onclick = function () {
-        if (!one) {
-            answer.innerHTML === '0' ? answer.innerHTML = button.innerHTML : answer.innerHTML += button.innerHTML
+    button.addEventListener('click', function () {
+        if (!operandOne) {
+            answer.textContent === '0' ? answer.textContent = button.textContent : answer.textContent += button.textContent
+        } else {
+            operandTwo ? operandTwo = answer.textContent += button.textContent : operandTwo = answer.textContent = button.textContent
         }
-        if (one) {
-            if (!two) {
-                two = answer.innerHTML = button.innerHTML
-            } else {
-                two = answer.innerHTML += button.innerHTML
-            }
+        if (answer.textContent.length > 7) {
+            answer.textContent = answer.textContent.slice(0, 7)
         }
-        if (answer.innerHTML.length > 7) {
-            answer.innerHTML = answer.innerHTML.slice(0, 7)
-        }
-    }
+    })
 }
 
-div.onclick = function() {
+div.addEventListener('click', function () {
     operation = 'div'
-    operationCalc(operation)
-}
+    isOperationCalc(operation)
+})
 
-multi.onclick = function() {
+multi.addEventListener('click', function () {
     operation = 'multi'
-    operationCalc(operation)
-}
+    isOperationCalc(operation)
+})
 
-sub.onclick = function() {
+sub.addEventListener('click', function () {
     operation = 'sub'
-    operationCalc(operation)
-}
+    isOperationCalc(operation)
+})
 
-sum.onclick = function() {
+sum.addEventListener('click', function () {
     operation = 'sum'
-    operationCalc(operation)
-}
+    isOperationCalc(operation)
+})
 
-function operationCalc(operation) {
-    if(one && two && operation) {
-        answer.innerHTML = calc(operation, one, two)
+function isOperationCalc(operation) {
+    const isValidOperand = operandOne && operandTwo && operation
+    if (isValidOperand) {
+        answer.textContent = calc(operation, operandOne, operandTwo)
     }
-    if (!one) {
-        one = answer.innerHTML
+    if (!operandOne) {
+        operandOne = answer.textContent
     }
 }
 
@@ -71,34 +67,47 @@ function calc(operation, one, two) {
     return isFinite(result) ? result : 'Ошибка'
 }
 
-result.onclick = function () {
-    if (one && two && operation) {
-        answer.innerHTML = calc(operation, one, two)
-        one = answer.innerHTML
-        if (answer.innerHTML.length > 7) {
-            answer.innerHTML = (+answer.innerHTML).toExponential(1)
+result.addEventListener('click', resultCalc)
+
+function resultCalc() {
+    const isValidOperand = operandOne && operandTwo && operation
+    if (isValidOperand) {
+        answer.textContent = calc(operation, operandOne, operandTwo)
+        operandOne = answer.textContent
+        operandOne = null
+        operandTwo = null
+        operation = undefined
+        if (answer.textContent.length > 7) {
+            answer.style.fontSize = '28px'
         }
     }
 }
 
-deleteNumber.onclick = function () {
-    if (answer.innerHTML.length === 1) {
-        answer.innerHTML = '0'
-        one = null
-        two = null
+
+deleteNumber.addEventListener('click', deleteNum)
+
+function deleteNum() {
+    if (answer.textContent.length === 1) {
+        answer.textContent = '0'
+        operandOne = null
+        operandTwo = null
         operation = null
-    } else if(!operation) {
-        answer.innerHTML = answer.innerHTML.slice(0,-1)
-    } else if(operation) {
-        two = two.slice(0, -1)
-        answer.innerHTML = two
+        answer.style.fontSize = '80px'
+    } else if (!operation) {
+        answer.textContent = answer.textContent.slice(0, -1)
+    } else if (operation) {
+        operandTwo = operandTwo.slice(0, -1)
+        answer.textContent = operandTwo
     }
 }
 
-reload.onclick = function () {
-    answer.innerHTML = '0'
+reload.addEventListener('click', resetCalc)
+
+function resetCalc() {
+    answer.textContent = '0'
     operation = null
-    one = null
-    two = null
+    operandOne = null
+    operandTwo = null
+    answer.style.fontSize = '80px'
 }
 
